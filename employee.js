2d5ebbf03c93d657165c7ce566ecbf85c2865b96
Zack <zmoumen13@gmail.com>
2018-02-04 13:40:50 -0700

    changes to moment.js functionality

--- employee.js
+++ employee.js
@@ -21,53 +21,22 @@ $(document).ready(function () {
 	var role = "";
 	var dateStarted;
 	var rate;
-	var presentDate = 01022018;
 	var monthsWorked;
 	var totalBilled;
 
-	database.ref().on("child_added", function(snapshot) {
-
-		console.log(snapshot.val())
-
-		// if (snapshot.child("employeeName").exists() && snapshot.child("role").exists() && snapshot.child("rate").exists()
-		// && snapshot.child("monthsWorked").exists() && snapshot.child("monthsWorked").exists()) {
-
-		employeeName = snapshot.val().employeeName;
-	    role = snapshot.val().role;
-	    rate = parseInt(snapshot.val().rate);
-	    monthsWorked = parseInt(snapshot.val().monthsWorked);
-	    dateStarted = parseInt(snapshot.val().dateStarted);
-	    presentDate = parseInt(snapshot.val().presentDate);
-	    totalBilled = rate * monthsWorked;
-
-
-		$("#employee-table > tbody").append("<tr><td>" + employeeName + "</td><td>" + role + "</td><td>" + dateStarted + "</td><td>" 
-			+ monthsWorked + "</td><td>" +  rate + "</td><td>" + totalBilled + "</td></tr>");
-
-		// }
-
-	}), function(errorObject) {
-	  console.log("The read failed: " + errorObject.code);
-	};
-
-
-		// functionality for when user submits form
+// functionality for when user submits form
 
 
 	$("#formSubmit").on("click", function() {
 
 		event.preventDefault();
 
-		// $("#my-form")[0].reset();
-
 		if (true) {
 
 			employeeName = $("#employeeForm").val().trim();
 			role = $("#roleForm").val().trim();
-			dateStarted = $("#startForm").val().trim();
+			dateStarted = moment($("#startForm").val().trim(), "DD/MM/YY").format("X"); 
 			rate = parseInt($("#rateForm").val().trim());
-			monthsWorked = presentDate - dateStarted;
-			totalBilled = monthsWorked * rate;
 
 			database.ref().push({
 				employeeName: employeeName,
@@ -79,6 +48,8 @@ $(document).ready(function () {
 			console.log(employeeName);
 			console.log(role);
 			console.log(rate);
+			console.log(String.dateStarted);
+
 
 			$("#employeeForm").val("");
 			$("#roleForm").val("");
@@ -92,6 +63,34 @@ $(document).ready(function () {
 
 	});
 
+
+	//pushs database values into Firebase
+
+	database.ref().on("child_added", function(snapshot) {
+
+		console.log(snapshot.val())
+
+		// if (snapshot.child("employeeName").exists() && snapshot.child("role").exists() && snapshot.child("rate").exists()
+		// && snapshot.child("monthsWorked").exists() && snapshot.child("monthsWorked").exists()) {
+
+		employeeName = snapshot.val().employeeName;
+	    role = snapshot.val().role;
+	    rate = parseInt(snapshot.val().rate);
+	    dateStarted = snapshot.val().dateStarted;
+
+	    formatDate = moment.unix(dateStarted).format("MM/DD/YY"); 
+	    monthsWorked = moment().diff(moment.unix(dateStarted, "X"), "months"); 
+	    totalBilled = rate * monthsWorked;
+
+
+		$("#employee-table > tbody").append("<tr><td>" + employeeName + "</td><td>" + role + "</td><td>" + formatDate + "</td><td>" 
+			+ monthsWorked + "</td><td>$" +  rate + "</td><td>" + totalBilled + "</td></tr>");
+
+		// }
+
+	}), function(errorObject) {
+	  console.log("The read failed: " + errorObject.code);
+	};
 
 
 });
